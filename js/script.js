c6f7e66b8bfcded4ee83f1425e78f986e755c48b
Frank Sam Hu <fsh1991@hotmail.com>
2015-01-14 23:27:42 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var apiKey = '2ZMRx5Wr9KOKm3hCKTfc4Q';
+var m = new mandrill.Mandrill(apiKey);
+function sendTheMail() {
+	
+	var name = $("#nameField").val();
+	var company = $("#companyField").val();
+	var email = $("#emailField").val();
+	var messageString = $("#messageField").val();
+	$("#nameField").val("");
+	$("#companyField").val("");
+	$("#emailField").val("");
+	$("#messageField").val("");
+	var messageBody = "Name: " + name + "\nCompany: " + company + "\nMessage:\n\n"+messageString;
+	var params = {
+		"message": {
+			"from_email":email,
+			"to":[{"email":"fsh1991@hotmail.com"}],
+			"subject": "New contact mail",
+			"text": messageBody
+		}
+	};
+
+	m.messages.send(params, function(res) {
+		console.log(JSON.stringify(res));
+	}, function(err) {
+		console.log(JSON.stringify(err));
+	});
+}
